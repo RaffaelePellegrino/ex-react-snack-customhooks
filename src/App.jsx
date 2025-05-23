e6236a2c370ef14fb8673d7df8459c062fc4b8af
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2025-05-23 17:46:15 +0200

    milestone 2 timer set

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import useSwitch from "./useSwitch";
+import useDate from "./useDate";
 import "./App.css";
 
 function App() {
   const [isOn, toggle] = useSwitch();
-
+  const date = useDate();
   return (
     <>
       <h1>il valore è {isOn ? "ON" : "OFF"}</h1>
       <button onClick={toggle}>{isOn ? "OFF" : "ON"}</button>
+
+      <h2>{date.toLocaleString()}</h2>
     </>
   );
 }
